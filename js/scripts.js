adba5ff1e0cb98e04f971d4ff5c8bfcb0720b9a4
gabrielebottari <gabriele.bottari1@gmail.com>
2024-01-12 15:26:35 +0100

    FIX general minor issue w/style comment and indentation END exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,39 @@
-// inizializzo un oggetto globale luxon
+/*
+Boolzapp - a (not very) innovative messaging platform
+
+Nome Repo: vue-boolzapp
+Milestone 1
+Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi)
+e dall’interlocutore (bianco) assegnando due classi CSS diverse
+Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto
+ attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato
+
+Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra,
+come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta,
+che apparirà dopo 1 secondo.
+
+Milestone 4
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene
+le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
+Milestone 5 - opzionale
+Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti 
+
+Consigli utili:
+Si possono trascurare le scrollbar verticali, sia nel pannello dei messaggi, che nella lista dei contatti
+I pulsanti e le icone possono non funzionare (a parte l’invio del messaggio)
+Per gestire le date, può essere utile la libreria Luxon
+La struttura dell’array dei contatti potrebbe avere questa forma:
+*/
+
+//inizializzo una costante globale luxon
 const time = luxon.DateTime
 
 // Estraggo la funzione createApp dall'oggetto Vue
@@ -191,61 +226,64 @@ createApp({
   },
 
   methods: {
-
  
-        // Funzione stampa nuovo messaggio in send
-        newMessage() {
+    //funzione per stampare un nuovo messaggio
+    newMessage() {
 
-            //verifico se il messaggio non è vuoto prima di procedere
-            if (this.inputMessage.trim() !== '') {
+        //verifico se il messaggio non è vuoto prima di procedere
+        if (this.inputMessage.trim() !== '') {
 
-                const newMessage = {
-                    message: this.inputMessage,
-                    status: 'sent',
-                    date: time.now().setLocale('it').toFormat('dd/MM/yyyy HH:mm:ss'),
-                };
+            //il messaggio avrà il contenuto dell'input, status sent e l'orario formattato
+            const newMessage = {
+                message: this.inputMessage,
+                status: 'sent',
+                date: time.now().setLocale('it').toFormat('dd/MM/yyyy HH:mm:ss'),
+            };
               
-              //pulisco il campo di input dopo l'invio
-              this.inputMessage = '';
+            //pulisco il campo di input dopo l'invio
+            this.inputMessage = '';
               
-              //aggiungo il nuovo messaggio all'array dei messaggi
-              this.contacts[this.index].messages.push(newMessage);
+            //aggiungo il nuovo messaggio all'array dei messaggi
+            this.contacts[this.index].messages.push(newMessage);
               
-              //chiamo a responseMessage() per gestire eventuali risposte automatiche
-              this.responseMessage();
+            //chiamo a responseMessage() per gestire eventuali risposte automatiche
+            this.responseMessage();
             }
         },
-    
+        
+        //funzione per stampare un messaggio di risposta
         responseMessage(){
 
+            //il messaggio dopo 1 secodno verra inviato con status received e con l'orario formattato
             setTimeout(() => {
             const message = {
                 message: 'Ok',
                 status: 'received',
                 date: time.now().setLocale('it').toFormat('dd/MM/yyyy HH:mm:ss'),
                 }
+
+            //viene pushato nell'array    
             this.contacts[this.index].messages.push(message);
             }, 1000);
     
         },
 
+        //funzione per cercare tra i contatti
         search(){
 
+            //cerca i contatti in base al criterio di ricerca (deve includere le lettere nel nome)
             this.contacts.forEach(contact => {
             contact.visible = contact.name.toLowerCase().includes(this.contactSearch.toLowerCase())
-            console.log(this.contactSearch)
             })
 
         },
 
-        visibleContacts() {
-            return this.contacts.filter(contact => contact.visible);
-        },
-
+        //funzione per cancellare il messaggio selezionato
         deleteMessage(i){
             this.contacts[this.index].messages.splice(i,1);
         },
 
+        //funzione per formattare la data
         formatTime(dateTime) {
             const formattedDate = time.fromFormat(dateTime, 'dd/MM/yyyy HH:mm:ss', {
                  locale: 'it' 
